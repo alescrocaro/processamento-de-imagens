1a6f195d78aab3c00bfb9dcb53557ad56fafc46c
alescrocaro <alescrocaro@gmail.com>
2022-08-30 21:16:02 -0300

    inicio tarefa2

---
+++ tarefa2/vizinhos_mais_proximos.m
@@ -0,0 +1,20 @@
+img = imread('pratica2.jpg');
+
+sizeImg = size(img);
+
+%duas próximas linhas definem qual será o fator para criar a nova imagem%
+linhaNovaImg = sizeImg(1,1)/8;
+colunaNovaImg = sizeImg(1,2)/8;
+
+novaImg = uint8(zeros(round(linhaNovaImg),round(colunaNovaImg)));
+
+for coluna = 1:colunaNovaImg
+    for linha = 1:linhaNovaImg
+        coordLinha = ceil(linha*sizeImg(1,1)/linhaNovaImg);
+        coordColuna = ceil(coluna*sizeImg(1,2)/colunaNovaImg);
+        
+        novaImg(linha,coluna) = img(coordLinha, coordColuna);
+    end
+end
+
+imshow(novaImg);
