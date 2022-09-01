ed435c7b4c9d9a632642124568c6b310f18c7e69
alescrocaro <alescrocaro@gmail.com>
2022-08-31 22:28:24 -0300

    tarefa2

--- tarefa2/interpolacao_bilinear.m
+++ tarefa2/interpolacao_bilinear.m
@@ -1,22 +1,44 @@
-img = imread('pratica2.jpg');
+img=imread('pratica2.jpg');
 
 sizeImg = size(img);
 
-%duas próximas linhas definem qual será o fator para criar a nova imagem%
-linhaNovaImg = sizeImg(1,1)/8;
-colunaNovaImg = sizeImg(1,2)/8;
+linhaNovaImg = sizeImg(1,1)*10;
+colunaNovaImg = sizeImg(1,2)*10;
 
-novaImg = uint8(zeros(round(linhaNovaImg),round(colunaNovaImg)));
+%fator novo%
+fatorLinha = size(img,1)/linhaNovaImg;
+fatorColuna = size(img,2)/colunaNovaImg;
 
-for coluna = 1:colunaNovaImg
-    for linha = 1:linhaNovaImg
-        coordLinha = linha*sizeImg(1,1)/linhaNovaImg;
-        coordColuna = coluna*sizeImg(1,2)/colunaNovaImg;
+saida=zeros([linhaNovaImg,colunaNovaImg]);
 
+for linha = 1:linhaNovaImg
+    for coluna = 1:colunaNovaImg
+        fCoordColuna = floor(fatorColuna * coluna);
+        if(fCoordColuna < 1)
+            fCoordColuna = 1;
+        end
+        fCoordLinha = floor(fatorLinha * linha);
+        if(fCoordLinha < 1)
+            fCoordLinha = 1;
+        end
+        cCoordColuna = ceil(fatorColuna * coluna);
+        cCoordLinha = ceil(fatorLinha * linha);
         
+        x_fator = (fatorColuna * coluna) - fCoordColuna;
+        y_fator = (fatorLinha * linha) - fCoordLinha;
         
-        novaImg(linha,coluna) = img(coordLinha, coordColuna);
+        p1 = img(fCoordLinha, fCoordColuna);
+        p2 = img(fCoordLinha, cCoordColuna);
+        p3 = img(cCoordLinha, fCoordColuna);
+        p4 = img(cCoordLinha, cCoordColuna);
+      
+        pixel = p1 * (1 - x_fator) * (1 - y_fator) + p2 * x_fator * (1 - y_fator) + p3 * y_fator * (1 - x_fator) + p4 * x_fator * y_fator;
+        
+        saida(linha,coluna) = pixel;
     end
 end
 
-imshow(novaImg);
+
+saida = uint8(saida);
+  
+imshow(saida);
--- tarefa2/vizinhos_mais_proximos.m
+++ tarefa2/vizinhos_mais_proximos.m
@@ -3,8 +3,8 @@ img = imread('pratica2.jpg');
 sizeImg = size(img);
 
 %duas próximas linhas definem qual será o fator para criar a nova imagem%
-linhaNovaImg = sizeImg(1,1)/8;
-colunaNovaImg = sizeImg(1,2)/8;
+linhaNovaImg = sizeImg(1,1)*10;
+colunaNovaImg = sizeImg(1,2)*10;
 
 novaImg = uint8(zeros(round(linhaNovaImg),round(colunaNovaImg)));
 
